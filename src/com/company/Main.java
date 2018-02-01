3ffcd72629614e75a25ffba8305d5335081de758
Mantas <mantas.muzzz@gmail.com>
2018-02-01 09:14:17 +0200

    masyvo sk suma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,45 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     public static final int EXIT_CODE = 0;
+    private static int suma = 0;
+    private static int[] sk;
+
     public static void main(String[] args) {
         // write your code here
         Main obj = new Main();
+        obj.skaiciuIvedimas();
+        obj.rezultatas();
 
     }
-
-    public Main() {
-
-        int suma = 0;
-        int[] sk = new int[5];
-
-        for(int i = 0; i < sk.length; i++){
-            sk[i] = duokGarantuotaiSkaiciu();
-            suma += sk[i];
+    public void skaiciuIvedimas(){
+        System.out.println("Iveskite 5 skaicius");
+        Scanner in = new Scanner(System.in);
+        sk = new int[5];
+        for (int i = 0; i < sk.length; i++){
+            System.out.println("Iveskite skaiciu nr. " + (i + 1 ) );
+            sk[i] = in.nextInt();
+            suma = suma + sk[i];
         }
-        for(int i = 0; i < sk.length; i++) {
-            System.out.println((i + 1) + " skaicius " + sk[i]);
-
+    }
+    public void rezultatas(){
+        for (int i = 0; i < sk.length; i++){
+            System.out.println(sk[i]);
         }
         System.out.print("suma : " + suma);
-    }
-
-    public int duokGarantuotaiSkaiciu() {
-        int rezultatas = 0;
-        Scanner in = new Scanner(System.in);
-
-        boolean arPavyko = false;
-        while(arPavyko == false) {
-            try {
-                System.out.print("Iveskite skaiciu ");
-                rezultatas = in.nextInt();
-                arPavyko = true;
-            }catch (Exception klaida){
-                System.out.println("tekstas yra ne skaicius");
-                in.next();
-            }
-        }
-
-        return rezultatas;
-
     }
 }
